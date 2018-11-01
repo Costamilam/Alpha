ef10e899d39ecbacb74bd9b9287dbfd15aad6d82
Guilherme Costamilam <costamilam073@gmail.com>
2018-11-01 17:39:57 -0300

    Bug fix

--- src/DB.php
+++ src/DB.php
@@ -94,6 +94,14 @@ class DB
                     $type[] = 'b';
                     break;
 
+                case 'object':
+                    $parameter = 'Object';
+                    break;
+
+                case 'array':
+                    $parameter = 'Array';
+                    break;
+
                 default:
                     $type[] = null;
                     break;

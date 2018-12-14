cecc208604dbfab6948f5b73668e7eb0053b7eac
Guilherme Costamilam <guilherme@Inspiron.Costamilam>
2018-12-13 23:02:57 -0200

    application start timestamp removed

--- src/Request.php
+++ src/Request.php
@@ -4,17 +4,18 @@ namespace Costamilam\Alpha;
 
 class Request
 {
-    private static $time;
     private static $method;
+
     private static $header;
+
     private static $path;
+
     private static $param;
+
     private static $body;
 
     public static function load()
     {
-        self::$time = time();
-
         self::$method = strtoupper($_SERVER['REQUEST_METHOD']);
 
         //$route = preg_quote($route);
@@ -29,9 +30,7 @@ class Request
 
         self::$param = array();
 
-        $contentType = self::header('Content-Type');
-        $contentType = str_replace('/', '\/', $contentType);
-        if (preg_match('/'.$contentType.'/i', 'application/json')) {
+        if (strcasecmp(self::header('Content-Type'), 'application/json')) {
             self::$body = json_decode(file_get_contents('php://input'), true);
         } else {
             parse_str(file_get_contents('php://input'), self::$body);
@@ -43,27 +42,26 @@ class Request
         if (function_exists('apache_request_headers')) {
             self::$header = apache_request_headers();
         } else {
-            $arh = array();
-            $rx_http = '/\AHTTP_/';
+            self::$header = array();
+
             foreach ($_SERVER as $key => $val) {
-                if (preg_match($rx_http, $key)) {
-                    $arh_key = preg_replace($rx_http, '', $key);
-                    $rx_matches = array();
-                    $rx_matches = explode('_', $arh_key);
-                    if (count($rx_matches) > 0 and strlen($arh_key) > 2) {
-                        foreach($rx_matches as $ak_key => $ak_val) $rx_matches[$ak_key] = ucfirst($ak_val);
-                        $arh_key = implode('-', $rx_matches);
+                if (preg_match('/\AHTTP_/', $key)) {
+                    $key = preg_replace('/\AHTTP_/', '', $key);
+
+                    $matches = explode('_', $key);
+
+                    if (count($matches) > 0 && strlen($key) > 2) {
+                        foreach($matches as $index => $value) {
+                            $matches[$index] = ucfirst($value);
+                        }
+
+                        $key = implode('-', $matches);
                     }
-                    $arh[$arh_key] = $val;
+
+                    self::$header[$key] = $val;
                 }
             }
-            self::$header = $arh;
         }
-    }
-
-    public static function time()
-    {
-        return self::$time;
     }
 
     public static function method()
@@ -112,7 +110,7 @@ class Request
         if (count($index) === 0) {
             return self::$param;
         } else {
-            return self::getIndex(self::$param, $index);
+            return self::getByIndex(self::$param, $index);
         }
     }
 
@@ -121,11 +119,11 @@ class Request
         if (count($index) === 0) {
             return self::$body ?: array();
         } else {
-            return self::getIndex(self::$body, $index);
+            return self::getByIndex(self::$body, $index);
         }
     }
 
-    private static function getIndex($array, $index)
+    private static function getByIndex($array, $index)
     {
         $data = array();
 
